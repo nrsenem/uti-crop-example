57c1c198c5d1647205142989ad3bcc88478b7a42
nursenem <220229025@kocaeli.edu.tr>
2025-07-09 11:02:05 -0400

    feat: Package Model Update

---
+++ src/executors/CropExampleExecutor.py
@@ -0,0 +1,44 @@
+"""
+    It is one of the preprocessing components in which the image is rotated.
+"""
+
+import os
+import cv2
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
+
+from sdks.novavision.src.media.image import Image
+from sdks.novavision.src.base.component import Component
+from sdks.novavision.src.helper.executor import Executor
+from components.CropExample.src.utils.response import build_response
+from components.CropExample.src.models.PackageModel import PackageModel
+
+
+class CropExampleExecutor(Component):
+    def __init__(self, request, bootstrap):
+        super().__init__(request, bootstrap)
+        self.request.model = PackageModel(**(self.request.data))
+        self.rotation_degree = self.request.get_param("Degree")
+        self.keep_side = self.request.get_param("KeepSide")
+        self.image = self.request.get_param("inputImage")
+
+    @staticmethod
+    def bootstrap(config: dict) -> dict:
+        return {}
+
+    def crop(self,img):
+        return  img[50:180, 100:300]
+
+
+
+    def run(self):
+        img = Image.get_frame(img=self.image, redis_db=self.redis_db)
+        img.value = self.crop(img.value)
+        self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
+        packageModel = build_response(context=self)
+        return packageModel
+
+
+if "__main__" == __name__:
+    Executor(sys.argv[1]).run()
--- src/utils/response.py
+++ src/utils/response.py
@@ -1,15 +1,15 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from components.CropExample.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, CropExampleExecutorOutputs, CropExampleExecutorResponse, CropExampleExecutor, OutputImage
 
 
 def build_response(context):
     outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
-    packageConfigs = PackageConfigs(executor=executor)
+    cropExampleExecutorOutputs= CropExampleExecutorOutputs(outputImage=outputImage)
+    cropExampleExecutorResponse = CropExampleExecutorResponse(outputs=cropExampleExecutorOutputs)
+    cropExampleExecutor=CropExampleExecutor(value=cropExampleExecutorResponse)
+    configexecutor = ConfigExecutor(value=cropExampleExecutor)
+    packageConfigs = PackageConfigs(executor=configexecutor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
     return packageModel
